ee64c83fed12d62c215fb20eb6dd9a29c65b9558
Tuân Đỗ <82562559+Tuanpluss02@users.noreply.github.com>
2021-10-13 14:30:24 +0700

    Create MyTemplate.cpp
    ¯\_(ツ)_/¯

---
+++ Template/MyTemplate.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define faster()                \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(NULL);                \
+  cout.tie(NULL);
+#define test() \
+  int test;    \
+  cin >> test; \
+  while (test--)
+#define pb push_back
+#define f first
+#define s second
+#define Mod 1000000007
+#define ld long double
+#define ll long long
+#define lli unsigned long long int
+#define For(i, a, b) for (int i = a; i ^ b; ++i)
+#define Forr(i, a, b) for (int i = a; i >= b; --i)
+#define vec(c) vector<c>
+#define uset(c) unordered_set<c>
+#define umap(c) unordered_map<c, c>
+#define sortu(c) sort(c.begin(), c.end())
+#define sortd(c) sort(c.rbegin(), c.rend())
+#define rev(c) reverse(c.begin(), c.end())
+#define pause() system("pause");
+
+using namespace std;
+
+int main()
+{
+  faster();
+  test()
+  {
+  }
+  pause();
+  return 0;
+}
+/**
+ * Tuan Plus
+ * De cho dep thoi chu khong biet code (●'◡'●)
+ * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐  ┌───┬───┬───┐
+ * │Esc│   │ F1│ F2│ F3│ F4│ │ F5│ F6│ F7│ F8│ │ F9│F10│F11│F12│ │P/S│S L│P/B│  │ B │ U │ G │
+ * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘  └───┴───┴───┘
+ * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
+ * │~ `│! 1│@ 2│# 3│$ 4│% 5│^ 6│& 7│* 8│( 9│) 0│_ -│+ =│ BacSp │ │Ins│Hom│PUp│ │N L│ / │ * │ - │
+ * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
+ * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │{ [│} ]│ | \ │ │Del│End│PDn│ │ 7 │ 8 │ 9 │   │
+ * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
+ * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │: ;│" '│ Enter  │               │ 4 │ 5 │ 6 │   │
+ * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
+ * │ Shift  │ Z │ X │ C │ V │ B │ N │ M │< ,│> .│? /│  Shift   │     │ ↑ │     │ 1 │ 2 │ 3 │   │
+ * ├─────┬──┴─┬─┴──┬┴───┴───┴───┴───┴───┴──┬┴───┼───┴─────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤ E││
+ * │ Ctrl│Win │Alt │         Space         │ Alt│ Fix Bug │Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │←─┘│
+ * └─────┴────┴────┴───────────────────────┴────┴─────────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
+ */
